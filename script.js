32a55e68fbfe16f442e01c6d1f07b0d137d6cdff
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-27 23:50:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    event.preventDefault(); 
+
+    let radius = parseFloat(document.getElementById("radius").value);
+    let volumeField = document.getElementById("volume");
+
+    if (isNaN(radius) || radius < 0) {
+        volumeField.value = "NaN"; 
+        return;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    
+    volumeField.value = volume.toFixed(4);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
